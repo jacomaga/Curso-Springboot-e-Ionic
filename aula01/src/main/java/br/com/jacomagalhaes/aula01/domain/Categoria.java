fa982e09395f2db663260fc456eee96d67746a6c
Jacó Magalhães <jacomagalhaes@gmail.com>
2020-11-24 11:05:46 -0300

    Base de dados H2 e criação automatizada da base de dados

---
+++ aula01/src/main/java/br/com/jacomagalhaes/aula01/domain/Categoria.java
@@ -0,0 +1,57 @@
+package br.com.jacomagalhaes.aula01.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    public Categoria(Integer id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Categoria() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
