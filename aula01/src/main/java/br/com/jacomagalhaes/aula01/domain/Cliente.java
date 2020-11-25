98ddfbd9aaab9b6ac0a03319b4e8338ac0680308
Jacó Magalhães <jacomagalhaes@gmail.com>
2020-11-25 05:07:34 -0300

    Usando apenas jsonignore

--- aula01/src/main/java/br/com/jacomagalhaes/aula01/domain/Cliente.java
+++ aula01/src/main/java/br/com/jacomagalhaes/aula01/domain/Cliente.java
@@ -2,6 +2,7 @@ package br.com.jacomagalhaes.aula01.domain;
 
 import br.com.jacomagalhaes.aula01.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
@@ -19,11 +20,11 @@ public class Cliente implements Serializable {
     private String cpfOuCnpj;
     private Integer tipo;
 
-    @JsonBackReference
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
-    @JsonManagedReference
+    
     @OneToMany(mappedBy = "cliente")
     private List<Endereco> enderecos = new ArrayList<>();
 
